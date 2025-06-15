bb76b37593091217b1a7ff1c8f3d8cd73094ac6c
nandana-ar <150732159+nandana-ar@users.noreply.github.com>
2025-06-15 22:50:16 +0100

    Solved Problem 268 Attempt 2 - Missing Number - Java

--- Java/MissingNumber.java
+++ Java/MissingNumber.java
@@ -2,6 +2,7 @@
 //                             MISSING NUMBER                            //
 
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Attempt #1~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 // Logic: 
 // -> Create a set
 // -> Add elements from the array to the set using a single loop 
@@ -37,4 +38,41 @@ class Solution {
 // Time Complexity: 
 // -> Building the set: O(n)
 // -> Checking numbers from 0 to n: O(n)
+// => O(n)
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Attempt #2~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// Logic: 
+// -> Calculate the expected sum of numbers from 0 to n using formula n*(n+1)/2
+// -> Calculate the actual sum of the numbers present in the array
+// -> The missing number is the difference between the expected sum and actual sum
+// -> Return that difference
+// -> If the expected and actual sum are equal, return 0 
+
+
+class Solution {
+    public int missingNumber(int[] nums) {
+
+        int n = nums.length; 
+
+        int expectedSum = n * (n + 1)/2 ;
+        int actualSum = 0; 
+
+        for (int i = 0; i < n; i++){
+            actualSum += nums[i]; 
+        }
+
+        if (actualSum == expectedSum){
+            return 0; 
+        } else { 
+            return expectedSum - actualSum; 
+        }
+    
+    }
+}
+
+// Time Complexity: 
+// -> Calculating the expected sum: O(1)
+// -> Calculating the actual sum using loop: O(n)
+// Overall, O(1) + O(n) 
 // => O(n)
