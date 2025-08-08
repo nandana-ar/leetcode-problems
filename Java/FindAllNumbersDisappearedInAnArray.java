1321c4d6fd565b7a18bdab1c257b55a8c99a00bd
nandana-ar <nandana.arun678@outlook.com>
2025-08-08 22:30:07 +0100

    Solved Problem 448 Attempt 2 - Find All Numbers Disappeared In An Array - Java

--- Java/FindAllNumbersDisappearedInAnArray.java
+++ Java/FindAllNumbersDisappearedInAnArray.java
@@ -2,6 +2,15 @@
 //              FIND ALL NUMBERS DISAPPEARED IN AN ARRAY                 //
 
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Attempt #1~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// Logic: 
+// -> Create a result list to store missing numbers
+// -> Sort the input array
+// -> For each number from 1 to n, check if it exists in the sorted array
+// -> If it does not exist, add it to the result list
+// -> Return the result list
+
+
 import java.util.*;
 
 class Solution {
@@ -33,3 +42,52 @@ class Solution {
 //     => O(n) * (O(log n) + O(1)) = O(n log n)
 // Overall, O(n log n) + O (n log n)  
 // => O(n log n)
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Attempt #2~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// Logic: 
+// -> Create a result list to store missing numbers
+// -> Iterate through the input array
+// -> For each number, mark its corresponding index as negative
+// -> After marking, iterate through the array again
+// -> If an index is still positive, it means the number i.e. index + 1 is missing
+// -> Add that number to the result list
+// -> Return the result list
+
+
+import java.util.*;
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        
+        List<Integer> result = new ArrayList<>(); 
+
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1; 
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
+        }
+
+        for (int j = 0; j < nums.length; j++) {
+            if(nums[j] > 0) {
+                result.add(j + 1);
+            }
+        }
+        
+        return result; 
+        
+    }
+}
+
+
+// Time Complexity: 
+// -> For each number in the array:
+//     - Marking the index as negative: O(1)
+//     => O(n) * O(1) = O(n)
+// -> After marking, iterating through the array again:
+//     - Checking if the index is positive: O(1)
+//     => O(n) * O(1) = O(n)
+// Overall, O(n) + O(n) 
+// => O(n)
